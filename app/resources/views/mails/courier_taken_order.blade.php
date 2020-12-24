7b9409c7e08a6b16269e71e5fdabe93d944ce50b
Nikita Gudkov <nikitaster2001@gmail.com>
2020-12-24 21:47:28 +0300

    add styles to mail templates

--- app/resources/views/mails/courier_taken_order.blade.php
+++ app/resources/views/mails/courier_taken_order.blade.php
@@ -10,33 +10,76 @@
         }
     </style>
 
+<div class="container">
+
     <h1>Заказ (#{{$order->id}}) уже в пути</h1>
     <p><strong>{{$order->fio}}</strong>, курьер уже в пути! Ваш заказ (#{{$order->id}}) на сумму <strong>{{$order->total}} ₽ </strong> из "{{$order->restaurant->name}}" скоро будет у вас!</p>
     <p>Связаться с курьером ({{$courier->fio}}): {{$courier->phone}}</p>
     
-    <br>
-    <h3>Ваш заказ:</h3>
-    <table>
-        <thead>
-            <tr>
-            <th scope="col">Позиция</th>
-            <th scope="col">Количество</th>
-            <th scope="col">Итого</th>
-            </tr>
-        </thead
-        <tbody>
-            @php
-            $positions = json_decode($order->goods);
-            foreach($positions as $position) {
-                echo    '<tr>' . '<th scope="row">' . $position[0]->name . '</th>' .
-                    '<td>' . $position[1] . '</td>' .
-                    '<td>' . (int)$position[0]->price * (int)$position[1] . ' ₽' . '</td>' .
-                '</tr>';
-            }
-            echo '<tr><th>Итого:</th><td></td><td>' . $order->total . ' ₽' . '</td></tr>';
-            @endphp
-        </tbody>
-    </table>
+    <div class="modal-dialog">
+            <div class="modal-header">
+            <a href="{{route('orders.show', $order->id)}}"><h3 class="modal-title">Заказ #{{$order->id}}</h3></a>
+            </div>
+            <div>
+                <h3>Доставка из {{$order->restaurant->name}}:</h3>
+            </div>
 
-    <a href="{{route('orders.show', $order->id)}}">Подробнее</a>
+            <!-- /.modal-header -->
+            <div class="modal-body">
+                @foreach(json_decode($order->goods) as $position)
+                    <div class="food-row food-row-order-confirmation">
+                        <span class="food-name">{{$position[0]->name}}</span>
+                        <strong class="food-price">{{$position[0]->price}} ₽</strong>
+                        <div class="food-counter">
+                            <span class="counter">Количество: {{$position[1]}}</span>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+
+            <div>
+                <h3>Контактные данные:</h3>
+            </div>
+            <div class="modal-body">
+                <div class="form-confirmation">
+                    <span>ФИО:</span>
+                    <div style="margin-bottom: 12px">
+                        <input type="text" class="input input-fio" value="{{$order->fio}}" disabled>
+                    </div> 
+
+                    <span>Email:</span>
+                    <div style="margin-bottom: 12px;">
+                        <input type="email" class="input input-email" value="{{$order->email}}" disabled>
+                    </div> 
+
+                    <span>Телефон:</span>
+                    <div style="margin-bottom: 12px;">
+                        <input type="tel" class="input input-phone" value="{{$order->tel}}" disabled>
+                    </div> 
+
+                    <span>Адрес доставки:</span>
+                    <div>
+                        <input type="text" class="input input-address-delivery" value="{{$order->address}}" disabled>
+                    </div> 
+                </div>
+            </div>
+            <input type="hidden" value="{{$order->id}}" name="order_on_queue">
+            <!-- /.modal-body -->
+            <div class="modal-footer">
+                <span class="modal-price-tag order-total">{{$order->total}} ₽</span>
+                <div class="footer-buttons">
+                    @auth
+                        @if(Auth::user()->account->role->name == 'courier')
+                            <form action="{{route('order_complete', $order->id)}}" method="POST">
+                                {{csrf_field()}}
+                                <button type="submit" class="button button-primary" name="order" value="{{$order->id}}">Завершить доставку</button>
+                            </form>
+                        @endif
+                    @endauth
+                </div>
+            </div>
+            <!-- /.modal-footer -->
+        </div>
+
+        </div>
 @endsection
--- app/resources/views/mails/order_complete.blade.php
+++ app/resources/views/mails/order_complete.blade.php
@@ -9,11 +9,14 @@
             text-decoration: none;
         }
     </style>
+    <div class="container">
 
-    <h1>Заказ (#{{$order->id}}) завершен!</h1>
-    <p><strong>{{$order->fio}}</strong>, спасибо за ваш заказ! Приятного аппетита!</p>
-    
-    <span>Пожалуйста, оцените ресторан "{{$order->restaurant->name}}" по ссылке: </span>
-    <a href="{{route('rate', $rate_token)}}">Оценить</a>
+        <h1>Заказ (#{{$order->id}}) завершен!</h1>
+        <p><strong>{{$order->fio}}</strong>, спасибо за ваш заказ! Приятного аппетита!</p>
+        
+        <span>Пожалуйста, оцените ресторан "{{$order->restaurant->name}}" по ссылке: </span>
+        <a href="{{route('rate', $rate_token)}}">Оценить</a>
+
+    </div>
 
 @endsection
